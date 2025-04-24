2bd4033f6774ce2f481bdd92585b1adbdcf42c57
fati <fatemesadatsedigh@gmail.com>
2025-04-24 12:20:27 +0330

    Add file opening functionality with error handling

--- src/editot/FileManager.java
+++ src/editot/FileManager.java
@@ -1,17 +1,30 @@
 package editor;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class FileManager {
-    public static void openFile(TextEditor textEditor, JTextArea textArea) {
+    public static void openFile(editor.TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            textEditor.currentFile = file;
+            textEditor.setTitle("Simple Text Editor - " + file.getName());
+
+            try {
+                String content = new String(Files.readAllBytes(file.toPath()));
+                textArea.setText(content);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(textEditor,
+                        "Error opening file: " + e.getMessage(),
+                        "Error", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
-    public static void saveFile(TextEditor textEditor, JTextArea textArea) {
+    public static void saveFile(editor.TextEditor textEditor, JTextArea textArea) {
         if (textEditor.currentFile == null){
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
@@ -23,7 +36,7 @@ public class FileManager {
         }
     }
 
-    public static void newFile(TextEditor textEditor, JTextArea textArea) {
+    public static void newFile(editor.TextEditor textEditor, JTextArea textArea) {
         //TODO
     }
 }
